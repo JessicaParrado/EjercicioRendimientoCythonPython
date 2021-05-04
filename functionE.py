b85f38aa5a128e1b5df54178e7afdc78e773c6aa
JessicaParrado <64098594+JessicaParrado@users.noreply.github.com>
2021-05-04 18:18:15 -0500

    Taller Rendimiento Cython y Python
    Se agregan los programas utilizados y la imagen de la gráfica de rendimiento

---
+++ functionE.py
@@ -0,0 +1,26 @@
+'''
+*Universidad Sergio Arboleda
+*Autores: Jessica Valentina Parrado Alfonso
+Fecha:Mayo 2021
+Computación Paralela y Distribuida
+'''
+from math import exp
+import numpy as np
+
+def rbf_network(X, beta, theta):
+
+    N = X.shape[0]
+    D = X.shape[1]
+    Y = np.zeros(N)
+
+    for i in range(N):
+        for j in range(N):
+            r = 0
+            for d in range(D):
+                r += (X[j, d] - X[i, d]) ** 2
+            r = r**0.5
+            Y[i] += beta[j] * exp(-(r * theta)**2)
+
+    return Y
+
+
